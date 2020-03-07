f3df5a026a7b51f608f11b966a951b01a116dbf8
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-07 17:38:51 +0000

    create blank components and add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+import EditTodo from './components/EditTodo';
 
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <h2>Mern Stack ToDo App</h2>
-      </div>
+      <Router>
+        <div className='container'>
+          <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <Link to='/' className='navbar-brand'>
+              MERN-Stack Todo App
+            </Link>
+            <div className='collapse navbar-collapse' id='navbarNav'>
+              <ul className='navbar-nav'>
+                <li className='nav-item active'>
+                  <Link to='/' className='nav-link'>
+                    Todos
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link to='/create' className='nav-link'>
+                    Create Todo
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <Route path='/' exact component={TodoList} />
+          <Route path='/edit/:id' component={EditTodo} />
+          <Route path='/create' component={CreateTodo} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Todos List</p>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
