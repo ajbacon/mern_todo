4223a14156c8b96f9aa6526591a2955f94bb951b
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-07 18:41:15 +0000

    add on change methods

--- client/src/components/CreateTodo.js
+++ client/src/components/CreateTodo.js
@@ -1,6 +1,35 @@
 import React from 'react';
 
 class CreateTodo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todo_description: '',
+      todo_responsible: '',
+      todo_priority: '',
+      todo_completed: false
+    };
+  }
+
+  onChangeDescription = e => {
+    this.setState({
+      todo_description: e.target.value
+    });
+  };
+
+  onChangeResponsible = e => {
+    this.setState({
+      todo_responsible: e.target.value
+    });
+  };
+
+  onChangePriority = e => {
+    this.setState({
+      todo_priority: e.target.value
+    });
+  };
+
   render() {
     return (
       <div>
