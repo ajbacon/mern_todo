694ac2ba6b76883c7d6cbe916e7f09c598531fd8
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-08 09:16:17 +0000

    implement todo table

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { todos: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:4000/todos')
+      .then(res => this.setState({ todo: res.data }))
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  todoList = () => {
+    return this.state.todos.map((todo, i) => {
+      return <Todo todo={todo} key={i} />;
+    });
+  };
+
   render() {
     return (
       <div>
-        <p>Todos List</p>
+        <h3>Todos List</h3>
+        <table className='table table-striped' style={{ marginTop: 20 }}>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Responsible</th>
+              <th>Priority</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.todoList()}</tbody>
+        </table>
       </div>
     );
   }
